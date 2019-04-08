a02fd486717510c7b6bb0093665d142e9d76be61
Mikk Pavelson <mikk.pavelson@gmail.com>
2019-04-08 11:10:33 +0300

    init

---
+++ Rentacar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rentacar
+//
+//  Created by Mikk Pavelson on 08/04/2019.
+//  Copyright © 2019 Mikk Pavelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
