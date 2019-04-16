4c0a3e632d9514776394548ba265c184c4694396
Mikk Pavelson <mikk.pavelson@gmail.com>
2019-04-16 22:42:41 +0300

    yaml fix

--- RentacarUITests/RentacarUITests.swift
+++ RentacarUITests/RentacarUITests.swift
@@ -26,9 +26,16 @@ class RentacarUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMainView() {
+        let app = XCUIApplication()
+        
+        XCTAssertTrue(app.staticTexts["Rentacar"].exists)
+        XCUIApplication().buttons["Sign Up"].tap()
+        XCTAssertTrue(app.staticTexts["Signup"].exists)
+        XCTAssertTrue(app.staticTexts["first name"].exists)
+        XCTAssertTrue(app.staticTexts["last name"].exists)
+        XCTAssertTrue(app.staticTexts["email"].exists)
+        XCTAssertTrue(app.staticTexts["password"].exists)
     }
 
 }
